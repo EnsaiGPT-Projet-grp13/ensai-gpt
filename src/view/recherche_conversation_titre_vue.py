0637516756957c472845ac7f81909491b56ace73
id2728-ensai <laura.bardou@eleve.ensai.fr>
2025-11-05 15:13:52 +0000

    historique

--- src/view/recherche_conversation_titre_vue.py
+++ src/view/recherche_conversation_titre_vue.py
@@ -26,10 +26,24 @@ class RechercheConversationTitreVue(VueAbstraite):
         if not conversations:
             from view.menu_utilisateur_vue import MenuUtilisateurVue
             return MenuUtilisateurVue("Aucune conversation dans l'historique.")
-        titre = inquirer.text(message="Quel titre recherchez vous ? :").execute().strip().lower()
-        listes_titres = 
+        mots = inquirer.text(message="Quel titre recherchez vous ? :").execute().strip().lower()
+        listes_titres = ConversationService.recherche_mots_titre(id_utilisateur, mots)
 
-
-        choix = [f"{c.get('titre')} avec {c.get('personnage_name')} (id_conversation#{c.get('id_conversation')})" for c in conversations] + ["Retour"]
+        choix = [f"{c.get('titre')} avec {c.get('personnage_name')} (id_conversation#{c.get('id_conversation')})" for c in listes_titres] + ["Retour"]
         label = inquirer.select(message="Quelle conversation voulez-vous ?", choices=choix).execute()
+        if label == "Retour":
+            # Retourne vers la vue du menue de l'utilisateur
+            from view.historique_vue import HistoriqueVue
+            return HistoriqueVue()
+        if "#" in label and ")" in label:
+            pid = int(label.split("#")[-1].rstrip(")"))
+        else:
+            raise ValueError("Format de label incorrect, impossible d'extraire l'ID.")
+        conversation = next(c for c in conversations if c.get('id_conversation') == pid)
+        id_conversation = conversation.get('id_conversation')
+
+        conversation_choisie = service.get(id_conversation)
+
+        from view.parametre_conversation_vue import ParametreConversationVue
+        return ParametreConversationVue()
 
