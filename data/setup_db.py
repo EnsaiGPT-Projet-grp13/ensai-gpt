a3057480ba0d14941c425c7d39ab147b47b6bc52
feliciadossou <felicia.dossou@eleve.ensai.fr>
2025-11-06 18:22:36 +0000

    correction path setup_db

--- data/setup_db.py
+++ data/setup_db.py
@@ -2,9 +2,9 @@ import subprocess
 import sys
 
 # Exécute init_db.py
-subprocess.run([sys.executable, "init_db.py"], check=True)
+subprocess.run([sys.executable, "data/init_db.py"], check=True)
 
 # Exécute pop_db.py
-subprocess.run([sys.executable, "pop_db.py"], check=True)
+subprocess.run([sys.executable, "data/pop_db.py"], check=True)
 
 print("Base de données initialisée avec succès.")
