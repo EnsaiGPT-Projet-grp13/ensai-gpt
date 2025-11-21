d58c00c95496271a21c6b5854d063c72b215c2bf
feliciadossou <felicia.dossou@eleve.ensai.fr>
2025-11-21 19:41:37 +0000

    nettoyage objects

--- src/objects/utilisateur.py
+++ src/objects/utilisateur.py
@@ -1,36 +1,15 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+
+@dataclass
 class Utilisateur:
-    """
-    Classe représentant un Utilisateur
-
-    Attributs
-    ----------
-    id_utilisateur : int
-        identifiant
-    prenom : str
-        prénom de l'utilisateur
-    nom : str
-        nom de l'utilisateur
-    mdp : str
-        le mot de passe de l'utilisateur
-    naiss : str
-        date de naissance (au format YYYY-MM-DD)
-    mail : str
-        adresse mail de l'utilisateur
-    """
-
-    def __init__(self, prenom, nom, naiss, mail, mdp_hash=None, id_utilisateur=None):
-        """Constructeur"""
-        self.id_utilisateur = id_utilisateur
-        self.prenom = prenom
-        self.nom = nom
-        self.mdp_hash = mdp_hash
-        self.naiss = naiss
-        self.mail = mail
-
-    def __str__(self):
-        """Permet d'afficher les informations de l'utilisateur"""
-        return f"Utilisateur({self.prenom} {self.nom}, né(e) le {self.naiss})"
-
-    def as_list(self) -> list[str]:
-        """Retourne les attributs de l'utilisateur dans une liste"""
-        return [self.prenom, self.nom, self.naiss, self.mail]
+    id_utilisateur: Optional[int]
+    prenom: str
+    nom: str
+    mail: str
+    mdp_hash: str
+    naiss: Optional[datetime] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
