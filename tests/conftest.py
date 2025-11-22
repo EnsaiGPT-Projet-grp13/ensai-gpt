1831d39662f866f615e7f5d4f6fd594f1ca67ae7
feliciadossou <felicia.dossou@eleve.ensai.fr>
2025-11-22 00:24:25 +0000

    correction conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,27 +5,15 @@ from unittest.mock import patch
 
 import pytest
 
-
-# Ajoute src/ au PYTHONPATH pour que `dao`, `service`, `utils`, etc. soient trouvés
-PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))  # ../
+PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 SRC_PATH = os.path.join(PROJECT_ROOT, "src")
 
 if SRC_PATH not in sys.path:
     sys.path.insert(0, SRC_PATH)
-    
-# === Imports du projet ===
+
 from dao.utilisateur_dao import UtilisateurDao
 from objects.utilisateur import Utilisateur
 from utils.securite import hash_password
-
-# === Ajouter src/ au PYTHONPATH ===
-THIS_DIR = os.path.dirname(__file__)  # .../ensai-gpt/tests
-PROJECT_ROOT = os.path.abspath(os.path.join(THIS_DIR, ".."))  # .../ensai-gpt
-SRC_DIR = os.path.join(PROJECT_ROOT, "src")
-
-if SRC_DIR not in sys.path:
-    sys.path.insert(0, SRC_DIR)
-
 
 @pytest.fixture(scope="session", autouse=True)
 def environnement_tests():
@@ -35,8 +23,6 @@ def environnement_tests():
       réinitialiser la base pendant les tests.
     """
     schema = os.getenv("POSTGRES_SCHEMA", "projetGPT")
-
-
 
 
 @pytest.fixture
